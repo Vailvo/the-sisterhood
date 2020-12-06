33e7024086896b0421c6b56ea9062f904e082d4e
melnee <Melanie.Neilan@gmail.com>
2020-12-05 21:33:41 -0800

    Added Images, but they aren't loading
    Added images and some more styling. Help me VailbeWan KanobeWilliams, you're my only hope

--- src/components/Directory/index.js
+++ src/components/Directory/index.js
@@ -1,9 +1,33 @@
 import React from "react";
+import Events from "./../../assets/Events.jpg";
+import Join from "./../../assets/Join.jpg";
+import "./styles.scss";
 
 const Directory = props => {
     return (
-        <div>
-
+        <div className = "directory">
+            <div className = "wrap">
+                <div
+                    className = "item"
+                    style = {{
+                        backgroundImage: 'url(' + { Events } + ")"
+                    }}
+                >
+                    <a>
+                        Events
+                    </a>
+                </div>
+                <div
+                    className = "item"
+                    style = {{
+                        backgroundImage: 'url(' + { Join } + ")"
+                    }}
+                >
+                    <a>
+                        Join
+                    </a>
+                </div>
+            </div>
         </div>
     );
 };
